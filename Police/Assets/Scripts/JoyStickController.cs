8d6966933d7917d0300ffedb5784343473ae6eed
Mert can Ünek <mrtcnunk@gmail.com>
2022-10-26 13:39:18 +0300

    update
    added character controls with joystick and created environment

---
+++ Police/Assets/Scripts/JoyStickController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStickController : MonoBehaviour
+{
+
+    public DynamicJoystick dynamicJoystick;
+    public float speed;
+    public float turnSpeed;
+
+    private void FixedUpdate()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            JoystickMovement();
+        }
+    }
+
+    private void JoystickMovement()
+    {
+        float Horizontal = dynamicJoystick.Horizontal;
+        float Vertical = dynamicJoystick.Vertical;
+        Vector3 addedPos = new Vector3(x: Horizontal * speed * Time.deltaTime, y: 0, z: Vertical * speed * Time.deltaTime);
+        transform.position += addedPos;
+
+        Vector3 direction = Vector3.forward * Vertical + Vector3.right * Horizontal;
+        transform.rotation = Quaternion.Slerp(a: transform.rotation, b: Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+    }
+}
